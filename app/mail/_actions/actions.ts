f476efd858e880d63338285b790c4da76c741533
nitesh kakkar <kakkar.nitesh04@gmail.com>
2024-12-30 15:15:29 +0530

    chore: mail actions

---
+++ app/mail/_actions/actions.ts
@@ -0,0 +1,63 @@
+"use server"
+
+import prisma from "@/utils/db";
+
+export async function getAcademicMails(userId : String)
+{
+    return await prisma.mail.findMany({
+        where: {
+            clerkId: userId,
+            category: "Academics"
+        },
+        orderBy : {updatedAt: 'desc'},
+        take: 5
+    });
+}
+
+export async function getCdcMails(userId : String)
+{
+    return await prisma.mail.findMany({
+        where: {
+            clerkId: userId,
+            category: "Cdc"
+        },
+        orderBy : {updatedAt: 'desc'},
+        take: 5
+    });
+}
+
+export async function getEventsMails(userId : String)
+{
+    return await prisma.mail.findMany({
+        where: {
+            clerkId: userId,
+            category: "Events"
+        },
+        orderBy : {updatedAt: 'desc'},
+        take: 5
+    });
+}
+
+export async function getHostelMails(userId : String)
+{
+    return await prisma.mail.findMany({
+        where: {
+            clerkId: userId,
+            category: "Hostel"
+        },
+        orderBy : {updatedAt: 'desc'},
+        take: 5
+    });
+}
+
+export async function getMiscMails(userId : String)
+{
+    return await prisma.mail.findMany({
+        where: {
+            clerkId: userId,
+            category: "Misc"
+        },
+        orderBy : {updatedAt: 'desc'},
+        take: 5
+    });
+}
